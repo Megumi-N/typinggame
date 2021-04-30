fbd1898e5767e6e0da8a7b5621020ea4803155b2
noguchi <02125011a@gmail.com>
2021-04-30 16:42:13 +0900

    [add]current_question and quesions for data, and set mounted:function

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ new Vue({
   el: "#app",
   data: {
     startFlg: "",
+    current_question: "",
+    questions: ["apple", "banana", "chocolate", "donut", "espresso"],
   },
   methods: {
     gameStart: function () {
       this.startFlg = true;
     },
+  },
+  mounted: function () {
+    this.current_question = this.questions[0];
   },
 });
