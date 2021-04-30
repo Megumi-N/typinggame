7fb7f8f14b0bdee3034adcf24aa2cd044f57aecd
noguchi <02125011a@gmail.com>
2021-04-30 16:49:41 +0900

    [add]set watch function, whether typeBox = questions or not

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ new Vue({
     startFlg: "",
     current_question: "",
     questions: ["apple", "banana", "chocolate", "donut", "espresso"],
+    typeBox: "",
   },
   methods: {
     gameStart: function () {
@@ -12,5 +13,14 @@ new Vue({
   },
   mounted: function () {
     this.current_question = this.questions[0];
+  },
+  watch: {
+    typeBox: function (e) {
+      if (e == this.current_question) {
+        this.questions.splice(0, 1); //配列の1つめを削除
+        this.current_question = this.questions[0];
+        this.typeBox = "";
+      }
+    },
   },
 });
