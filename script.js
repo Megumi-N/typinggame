ae05284bf61abb8267e97db83caaba1524d532ef
noguchi <02125011a@gmail.com>
2021-04-30 16:53:09 +0900

    [add]set count questions

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ new Vue({
     current_question: "",
     questions: ["apple", "banana", "chocolate", "donut", "espresso"],
     typeBox: "",
+    current_question_counts: 0,
+    question_counts: 0,
   },
   methods: {
     gameStart: function () {
@@ -13,6 +15,7 @@ new Vue({
   },
   mounted: function () {
     this.current_question = this.questions[0];
+    this.question_counts = this.questions.length;
   },
   watch: {
     typeBox: function (e) {
@@ -20,6 +23,7 @@ new Vue({
         this.questions.splice(0, 1); //配列の1つめを削除
         this.current_question = this.questions[0];
         this.typeBox = "";
+        this.current_question_counts = this.current_question_counts + 1;
       }
     },
   },
